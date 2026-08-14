a7c6bc9c3b30f001a6a467057c01b40933d7ee69
Bea Souza <158209681+souzabweatriz@users.noreply.github.com>
2026-08-13 22:38:51 -0300

    Aula 02

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(x: "Olá, Devops!");
+        System.out.println(x: "Pipeline executado com sucesso!");
+
+    }
+}
